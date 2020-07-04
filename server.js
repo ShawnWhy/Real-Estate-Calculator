f215f195008929f91a2397283e2f52136b610ae6
Shawn Yu <shawnyudesign@gmail.com>
2020-07-04 09:03:06 -0400

    added node modules

--- server.js
+++ server.js
@@ -2,25 +2,25 @@ require("dotenv").config();
 var express = require("express");
 const compression = require("compression");
 var session = require("express-session");
-var passport = require("./config/passport");
-var db = require("./models");
+// var passport = require("./config/passport");
+// var db = require("./models");
 var app = express();
 var PORT = process.env.PORT || 8080;
 // Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(compression());
 
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
 );
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+// require("./routes/apiRoutes")(app);
+// require("./routes/htmlRoutes")(app);
 var syncOptions = { force: false };
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
@@ -28,13 +28,13 @@ if (process.env.NODE_ENV === "test") {
   syncOptions.force = true;
 }
 // Starting the server, syncing our models ------------------------------------/
-db.sequelize.sync(syncOptions).then(function() {
-  app.listen(PORT, function() {
-    console.log(
-      "==> CONNECTED: Listening on port %s. Visit http://localhost:%s/ in your browser.",
-      PORT,
-      PORT
-    );
-  });
-});
+// db.sequelize.sync(syncOptions).then(function() {
+  // app.listen(PORT, function() {
+  //   console.log(
+  //     "==> CONNECTED: Listening on port %s. Visit http://localhost:%s/ in your browser.",
+  //     PORT,
+  //     PORT
+  //   );
+  // });
+// });
 module.exports = app;
