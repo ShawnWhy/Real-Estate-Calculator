c4f2d41c6b274607e529a8619aecbea5b6e1ffed
Shawn Yu <shawnyudesign@gmail.com>
2020-07-04 09:21:26 -0400

    typo

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use(compression());
 
 // We need to use sessions to keep track of our user's login status
-app.use(
-  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
-);
+// app.use(
+//   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+// );
 // app.use(passport.initialize());
 // app.use(passport.session());
 
@@ -28,12 +28,12 @@ app.get("*", (req, res) => {
 
 // require("./routes/apiRoutes")(app);
 // require("./routes/htmlRoutes")(app);
-var syncOptions = { force: false };
+// var syncOptions = { force: false };
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
-if (process.env.NODE_ENV === "test") {
-  syncOptions.force = true;
-}
+// if (process.env.NODE_ENV === "test") {
+//   syncOptions.force = true;
+// }
 // Starting the server, syncing our models ------------------------------------/
 // db.sequelize.sync(syncOptions).then(function() {
   // app.listen(PORT, function() {
